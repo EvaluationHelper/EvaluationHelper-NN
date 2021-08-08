6cff54a9b6449ad05f0c5f8e453f347ec89933f6
Nikita Nesterov <nik.nest1@gmail.com>
2021-08-08 16:12:16 +0200

    connected: corners detection & transformation estimation

--- src/create_reference.py
+++ src/create_reference.py
@@ -2,19 +2,21 @@ import argparse
 import time
 from utils.reference.corner_finder import CornerFinder
 from utils.reference.ReferenceSheet import ReferenceSheet
-from utils.reference.RotationTranslation import calculateRotationTranslation
 import json
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--detect_corners", default=True, type=bool, help="If True will detect corners, else will use already detected from default output")
+    parser.add_argument("-u", "--update_corners", action='store_true', help="If used will detect corners, else will use already detected from default output")
     parser.add_argument("-b", "--boegen_path", default="../data/boegen", type=str, help="Path to evaluations folder")
     parser.add_argument("-m", "--masks_path", default="../data/masks", type=str, help="Path to masks folder")
     parser.add_argument("-r", "--roi", type=str, default="../data/roi.json", help="Path to file containing roi")
     parser.add_argument("-oc", "--output_corners", type=str, default="../data/corners.json", help="Output json with detected corners for each boegen")
+    parser.add_argument("-ot", "--output_transformation", type=str, default="../data/transformations.json", help="Output json with rotation and translation for each boegen")
+    parser.add_argument("-a", "--annotated_bogen", type=str, default="../data/boegen/Bogen1.jpg", help="Path to annotated bogen, that will be used as reference")
     opt = parser.parse_args()
 
-    if opt.default_corners:
+    if opt.update_corners:
+        print("Update corners detection")
         f = open(opt.roi)
         roi = json.load(f)
         f.close()
@@ -42,12 +44,42 @@ if __name__ == '__main__':
             finish_time = time.perf_counter()
             print(f"Corners detection is finished in {finish_time - start_time} sec for {len(corners)} Boegen")
 
-            for boegen in corners:
-                pass
+            boegens = dict()
+            reference_corners = corners[opt.annotated_bogen]
+            reference_sheet = ReferenceSheet(reference_corners[0], reference_corners[1], reference_corners[2], reference_corners[3], [])
+            for boegen in corners.items():
+                cs = boegen[1]
+                name = boegen[0]
+                s = ReferenceSheet(cs[0], cs[1], cs[2], cs[3], [])
+                rot_matrix, translation, rot_angle_deg = reference_sheet.calculateRotationTranslation(s)
+                boegens[name] = {"rot_matrix" : rot_matrix.tolist(), "translation" : translation.tolist()}
+
+            f = open(opt.output_transformation, "w")
+            f.write(json.dumps(boegens))
+            f.close()
+            print(f"Transformations saved to {opt.output_transformation}")
         except Exception as e:
             print(e)
 
     else:
-        pass             
+        print("Use old corners detection")
+        f = open(opt.output_corners, "r")
+        corners = json.load(f)
+        f.close()
+
+        boegens = dict()
+        reference_corners = corners[opt.annotated_bogen]
+        reference_sheet = ReferenceSheet(reference_corners[0], reference_corners[1], reference_corners[2], reference_corners[3], [])
+        for boegen in corners.items():
+            cs = boegen[1]
+            name = boegen[0]
+            s = ReferenceSheet(cs[0], cs[1], cs[2], cs[3], [])
+            rot_matrix, translation, rot_angle_deg = reference_sheet.calculateRotationTranslation(s)
+            boegens[name] = {"rot_matrix" : rot_matrix.tolist(), "translation" : translation.tolist()}
+
+        f = open(opt.output_transformation, "w")
+        f.write(json.dumps(boegens))
+        f.close()
+        print(f"Transformations saved to {opt.output_transformation}")
 
 
