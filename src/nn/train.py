6434168c3fb2ff4cf9322c38828ca3d56a899efa
Nikita Nesterov <nik.nest1@gmail.com>
2021-08-12 01:03:35 +0200

    ADDED: train data; REMOVED: old code parts

--- src/nn/train.py
+++ src/nn/train.py
@@ -1,21 +1,21 @@
 import numpy as np
 import argparse
 from model import Model
-from dataset import DataLoader, DataSet, create_dataloader
+from dataset import create_bin_dataloader
 from loss import ComputeLoss
 
-def train(train_path, batch_size, epochs, learning_rate):
+def train(positive_path, negative_path, batch_size, epochs, learning_rate):
     # Model
     model = Model()
     
     # Train Loader
-    dataloader = create_dataloader(opt.train_path, opt.annotaion_path, opt.batch_size)
+    dataloader = create_bin_dataloader(positive_path, negative_path, batch_size)
 
     # Loss 
     compute_loss = ComputeLoss(model)
 
     # Training 
-    for epoch in range(opt.epochs):
+    for epoch in range(epochs):
 
         for i, (imgs, targets) in enumerate(dataloader): #=================== batch
 
@@ -26,16 +26,19 @@ def train(train_path, batch_size, epochs, learning_rate):
             # Backward
             model.backward(loss)
 
+            # Opimization
+            model.step(learning_rate)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--epochs", type=int, default=30, help="Num training epochs")
-    parser.add_argument("-b", "--batch_size", type=int, default=50, help="Num training epochs")
-    parser.add_argument("-t", "--train_path", type=str, default="../../data/boxes", help="Num training epochs")
-    parser.add_argument("-r", "--learning_rate", type=str, default="../../data/boxes", help="Num training epochs")
-    parser.add_argument("-a", "--annotaion_path", type=str, default="../../data/boxes", help="Num training epochs")
+    parser.add_argument("-e", "--epochs", type=int, default=30, help="")
+    parser.add_argument("-b", "--batch_size", type=int, default=50, help="")
+    parser.add_argument("-p", "--positive_path", type=str, default="", help="")
+    parser.add_argument("-n", "--negative_path", type=str, default="", help="")
+    parser.add_argument("-r", "--learning_rate", type=str, default="", help="")
+    parser.add_argument("-a", "--annotaion_path", type=str, default="", help="")
     opt = parser.parse_args()
     
 
-    train(opt.train_path, opt.batch_size, opt.epochs, opt.learning_rate)
+    train(opt.positive_path, opt.negative_path, opt.batch_size, opt.epochs, opt.learning_rate)
     
