9e52650f35f260f8937f7d4c606fa38a87123e9b
vietanng <59680678+vietanng@users.noreply.github.com>
2021-08-29 18:13:06 +0200

    Merge Private/johannes into master (#10)
    * Adjust old files
    
    * add corner_cutter.py
    
    * add boxes dir
    
    * fix rotation translation
    
    * Update RefSheet cornercutter
    
    * gitignore
    
    * Adjust old files
    
    * add corner_cutter.py
    
    * add boxes dir
    
    * fix rotation translation
    
    * Update RefSheet cornercutter
    
    * gitignore
    
    * create statistics
    
    * Fix average for statistics
    
    * Fix path, rename box class
    
    * Pydoc, fix minor naming conventions
    
    * gitignore

--- src/utils/reference/ReferenceSheet.py
+++ src/utils/reference/ReferenceSheet.py
@@ -1,13 +1,13 @@
-from utils.reference.RotationTranslation import calculateRotationTranslation, calculateRotatedTranslatedFromPoint
-from PIL import Image
+from src.utils.reference.RotationTranslation import calculateRotationTranslation, calculateRotatedTranslatedFromPoint
 
 
 class ReferenceSheet:
-    def __init__(self, upper_left, upper_right, bottom_left, bottom_right, *questions):
-        self._u_r = upper_right
+    def __init__(self, name, upper_left, upper_right, bottom_left, bottom_right, *questions):
+        self._name = name
         self._u_l = upper_left
-        self._b_r = bottom_right
+        self._u_r = upper_right
         self._b_l = bottom_left
+        self._b_r = bottom_right
         self._questions = questions
 
     def calculateRotationTranslation(self, target_ref_sheet):
@@ -15,15 +15,25 @@ class ReferenceSheet:
             :type target_ref_sheet: ReferenceSheet
         """
         return calculateRotationTranslation(
-            [self._u_l, self._u_r, self._b_r, self._b_l],
-            [target_ref_sheet._u_l, target_ref_sheet._u_r, target_ref_sheet._b_r, target_ref_sheet._b_l])
+            [self._u_l, self._u_r, self._b_l, self._b_r],
+            [target_ref_sheet._u_l, target_ref_sheet._u_r, target_ref_sheet._b_l, target_ref_sheet._b_r])
 
     def calculateRotatedTranslatedFromSheet(self, rotation_matrix, translation):
         return [calculateRotatedTranslatedFromPoint(point, rotation_matrix, translation)
-                for point in [self._u_l, self._u_r, self._b_r, self._b_l]]
+                for point in [self._u_l, self._u_r, self._b_l, self._b_r]]
+
+    def get_name(self):
+        return self._name
+
+    def get_questions(self):
+        return self._questions
+
+    def get_reference_points(self):
+        return self._u_l, self._u_r, self._b_l, self._b_r
 
     def asdict(self):
-        return {'referencePoints': {
+        return {'name': self._name,
+                'referencePoints': {
                     'upperLeft': self._u_l,
                     'upperRight': self._u_r,
                     'bottomLeft': self._b_l,
@@ -38,49 +48,48 @@ class Question:
         self._boxes = boxes
         self._answers = len(self._boxes)
 
+    def get_name(self):
+        return self._name
+
+    def get_boxes(self):
+        return self._boxes
+
     def asdict(self):
         return {self._name: {'boxes': [box.asdict() for box in self._boxes]}}
 
 
 class Box:
-    def __init__(self, upper_left, bottom_right):
+    def __init__(self, name, upper_left, bottom_right, path=None, is_checked=None):
+        self._name = name
         self._u_l = upper_left
         self._b_r = bottom_right
+        self._path = path
+        self._is_checked = is_checked
+
+    def get_name(self):
+        return self._name
+
+    def get_points(self):
+        return self._u_l, self._b_r
+
+    def get_is_checked(self):
+        return self._is_checked
+
+    def set_is_checked(self, new_is_checked):
+        self._is_checked = new_is_checked
+
+    def set_path(self, new_path):
+        self._path = new_path
+
+    def get_path(self):
+        return self._path
 
     def asdict(self):
         return {'upperLeft': self._u_l,
-                'bottomRight': self._b_r}
+                'bottomRight': self._b_r,
+                'path': self._path,
+                'is_checked': self._is_checked}
 
 
-if __name__ == "__main__":
 
-    # Data from coner_finder.py
-    referenceSheet1 = ReferenceSheet([1134, 388], [2354, 387], [1133, 2785], [2356, 2783], [])
-    referenceSheet28 = ReferenceSheet([1153, 390], [2374, 389], [1151, 2778], [2372, 2781], [])
 
-    # calculate rotation and translation for which ref1 must be rotated to fit ref28
-    # may need to adjust if the other way around is wanted
-    rot_matrix, translation, rot_angle_deg = referenceSheet1.calculateRotationTranslation(referenceSheet28)
-    print(rot_matrix)
-    print(translation)
-    print(rot_angle_deg)
-
-    image_ref_1 = Image.open(r"../data/boegen/Bogen1.jpg")
-    image_ref_28 = Image.open(r"../data/boegen/Bogen28.jpg")
-    image_ref_1_rot = image_ref_1.rotate(referenceSheet1.calculateRotationTranslation(referenceSheet28)[2])
-    image_ref_1_rot = image_ref_1_rot.transform(image_ref_1_rot.size, Image.AFFINE,
-                                                (1, 0, translation[0],
-                                                 0, 1, translation[1]))
-
-    image_ref_1.show()
-    image_ref_1_rot.show()
-    image_ref_28.show()
-
-    """
-    box1 = Box([0, 1], [1, 0])
-    box2 = Box([2, 1], [3, 0])
-    box3 = Box([4, 1], [5, 0])
-    lst = [box1, box2, box3]
-    question1 = Question('question1', box1, box2, box3)
-    question2 = Question('question2', box3, box2, box1)
-    """
---
+++ src/utils/reference/corner_cutter.py
@@ -0,0 +1,95 @@
+import json
+import os.path
+from src.utils.reference.ReferenceSheet import *
+from PIL import Image
+
+root = "../../../"
+
+
+def __read_sheet_json__(path):
+    """
+        create ReferenceSheet from json containing question and box positions
+
+        Args:
+            path: path to the sheet
+        Returns:
+            object of class src.utils.reference.ReferenceSheet.ReferenceSheet
+    """
+    file = open(path)
+    sheet_json = json.load(file)
+    file.close()
+
+    sheet_box_positions = sheet_json.get("boxPositions")
+    question_lst = []
+    for question_key in sheet_box_positions.keys():
+        question_lst.append(
+            Question(question_key,
+                     *[Box(box_key,
+                           sheet_box_positions.get(question_key).get(box_key).get("upperLeft"),
+                           sheet_box_positions.get(question_key).get(box_key).get("bottomRight"))
+                       for box_key in sheet_box_positions.get(question_key).keys()]))
+
+    return ReferenceSheet(sheet_json.get("name"),
+                          sheet_json.get("referencePoints").get("upperLeft"),
+                          sheet_json.get("referencePoints").get("upperRight"),
+                          sheet_json.get("referencePoints").get("bottomLeft"),
+                          sheet_json.get("referencePoints").get("bottomRight"),
+                          *question_lst)
+
+
+def __save_box__(img, box, path_suffix, box_path='data/boxes/', ext='.jpg', puffer=20, size=40):
+    """
+        crops image based on box coordinates, resizes and saves it
+
+        Args:
+            img: large image
+            box: object of class src.utils.reference.ReferenceSheet.Box
+            path_suffix: suffix for savename
+            box_path: path to boxes
+            ext: extension type
+            puffer: crop image with puffer around corners
+            size: resize size
+    """
+    puffer = 20
+    box_u_l, box_b_r = box.get_points()
+    img_crop = img.crop((box_u_l[0] - puffer, box_u_l[1] - puffer,
+                         box_b_r[0] + puffer, box_b_r[1] + puffer))
+    img_res = img_crop.resize((40, 40))
+
+    path = root + box_path + path_suffix + "_" + box.get_name() + ext
+    box.set_path(path)
+    img_res.save(path)
+
+
+def cut_boxes(corners_path, sheet_path='data/boegen/', reference_json_path='data/Bogen1ReferencePoints.json'):
+    """
+        rotates, translates, cuts and saves boxes based on corner data and reference sheet data
+
+        Args:
+            corners_path: path to json containing all sheets with their corners
+            sheet_path: path to sheet folder
+            reference_json_path: path to reference json
+    """
+    reference_sheet = __read_sheet_json__(root + reference_json_path)
+    file = open(corners_path)
+    corners = json.load(file)
+    file.close()
+
+    sheet_lst = [ReferenceSheet(key,
+                                corners.get(key)[0], corners.get(key)[1],
+                                corners.get(key)[2], corners.get(key)[3])
+                 for key in corners.keys()]
+
+    for sheet in sheet_lst:
+        # rotate and translate back to coordinates of ref_sheet1
+        rot_m, tl, rot_a = sheet.calculateRotationTranslation(sheet_lst[0])
+        image = Image.open(root + sheet_path + sheet.get_name())
+        image_rot_tl = image.transform(image.size, Image.AFFINE,
+                                       (rot_m[0][0], rot_m[0][1], tl[0],
+                                        rot_m[1][0], rot_m[1][1], tl[1]))
+
+        for question in reference_sheet.get_questions():
+            for box in question.get_boxes():
+                __save_box__(image_rot_tl, box,
+                             os.path.splitext(sheet.get_name())[0] + "_" + question.get_name())
+
